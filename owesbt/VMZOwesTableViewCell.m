27c30e99947239e3b55854419c6d210b99d7b4d7
Вячеслав Живетьев <slavyan6363@gmail.com>
2018-01-15 05:27:34 +0300

    исправлена ошибка с addSubview в init кастомной ячейки

--- owesbt/VMZOwesTableViewCell.m
+++ owesbt/VMZOwesTableViewCell.m
@@ -30,24 +30,24 @@
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if(self)
     {
-        _mainLabel = [[UILabel alloc] initWithFrame:CGRectMake(8, 8, CGRectGetWidth(self.frame) - 16, 21)];
+        _mainLabel = [[UILabel alloc] init];
         _mainLabel.textColor = [UIColor blackColor];
         _mainLabel.backgroundColor = [UIColor cyanColor];
         _mainLabel.text = @"Text";
-        [self addSubview:_mainLabel];
+        [self.contentView addSubview:_mainLabel];
         
         _secondLabel = [[UILabel alloc] initWithFrame:CGRectMake(8, 21+8, CGRectGetWidth(self.frame) - 16, 21)];
         _secondLabel.textColor = [UIColor blackColor];
         _secondLabel.backgroundColor = [UIColor cyanColor];
         _secondLabel.text = @"Text";
         _secondLabel.font = [UIFont systemFontOfSize:12];
-        [self addSubview:_secondLabel];
+        [self.contentView addSubview:_secondLabel];
         
         _emptyLabel = [[UILabel alloc] initWithFrame:CGRectMake(8, 8, CGRectGetWidth(self.frame) - 16, 21)];
         _emptyLabel.textAlignment = NSTextAlignmentCenter;
         _emptyLabel.textColor = [UIColor grayColor];
         _emptyLabel.text = @"Empty";
-        [self addSubview:_emptyLabel];
+        [self.contentView addSubview:_emptyLabel];
     }
     return self;
 }
