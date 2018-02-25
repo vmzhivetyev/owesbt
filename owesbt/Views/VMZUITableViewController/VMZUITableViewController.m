f2119251aa26c9279fecd5c14ad27f57e10f2ac6
Вячеслав Живетьев <slavyan6363@gmail.com>
2018-02-25 03:27:10 +0300

    добавлен класс VMZUITableViewController аналогичный UITableViewController

---
+++ owesbt/Views/VMZUITableViewController/VMZUITableViewController.m
@@ -0,0 +1,57 @@
+//
+//  VMZUITableViewController.m
+//  owesbt
+//
+//  Created by Вячеслав Живетьев on 20.02.2018.
+//  Copyright © 2018 Вячеслав Живетьев. All rights reserved.
+//
+
+#import "VMZUITableViewController.h"
+
+
+@interface VMZUITableViewController ()
+
+
+@end
+
+
+@implementation VMZUITableViewController
+
+
+#pragma mark - Lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    UITableView* tableView = [[UITableView alloc] initWithFrame:CGRectZero
+                                                          style:UITableViewStyleGrouped];
+    _tableView = tableView;
+    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    
+    self.tableView.allowsSelection = NO;
+    
+    [self.view addSubview:self.tableView];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
+}
+
+
+@end
